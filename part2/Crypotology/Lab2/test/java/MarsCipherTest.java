6f296600cf4a231e4ae30750b7fe5abdcc367ff4
Pilipets <glebon1802@gmail.com>
2020-05-02 03:36:42 +0300

    Added labs 2,3 on Cryptology. Implemented Mars Cipher. Implemented SHA-256 hash function. Added JUnit tests for added functionality.

---
+++ part2/Crypotology/Lab2/test/java/MarsCipherTest.java
@@ -0,0 +1,69 @@
+import cryptology.cipher.MarsCipher;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class MarsCipherTest {
+    final int NUMBER_OF_TESTS = 512;
+    final int[] KEY_RANGE = {16, 56}; // 128 bits - 448 bits
+    final int INPUT_MAX_LEN = 1000;
+
+    // function to generate a random string of length n
+    private static String getAlphaNumericString(int n)
+    {
+        // chose a Character random from this String
+        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                + "0123456789"
+                + "abcdefghijklmnopqrstuvxyz";
+
+        // create StringBuffer size of AlphaNumericString
+        StringBuilder sb = new StringBuilder(n);
+
+        for (int i = 0; i < n; i++) {
+            // generate a random number between
+            // 0 to AlphaNumericString variable length
+            int index
+                    = (int)(AlphaNumericString.length()
+                    * Math.random());
+
+            // add Character one by one in end of sb
+            sb.append(AlphaNumericString
+                    .charAt(index));
+        }
+        return sb.toString();
+    }
+
+    @Test
+    public void testMarsChars() {
+        Random rand = new Random();
+        for(int i = 0; i < NUMBER_OF_TESTS; ++i) {
+            String key = getAlphaNumericString(KEY_RANGE[0]
+                    + rand.nextInt(KEY_RANGE[1] - KEY_RANGE[0] + 1));
+            String input = getAlphaNumericString(rand.nextInt(INPUT_MAX_LEN));
+
+            byte[] encrypted = MarsCipher.encrypt(input.getBytes(), key.getBytes());
+            Assert.assertNotEquals(input, new String(encrypted));
+            byte[] decrypted = MarsCipher.decrypt(encrypted, key.getBytes());
+            Assert.assertEquals(input, new String(decrypted));
+        }
+    }
+
+    @Test
+    public void testMarsBytes() {
+        Random rand = new Random();
+        for(int i = 0; i < NUMBER_OF_TESTS; ++i) {
+            byte[] keyBytes = new byte[KEY_RANGE[0]
+                    + rand.nextInt(KEY_RANGE[1] - KEY_RANGE[0] + 1)];
+            rand.nextBytes(keyBytes);
+            byte[] inputBytes = new byte[rand.nextInt(INPUT_MAX_LEN)];
+            rand.nextBytes(inputBytes);
+
+            byte[] encrypted = MarsCipher.encrypt(inputBytes, keyBytes);
+            Assert.assertFalse(Arrays.equals(inputBytes, encrypted));
+            byte[] decrypted = MarsCipher.decrypt(encrypted, keyBytes);
+            Assert.assertTrue(Arrays.equals(inputBytes, decrypted));
+        }
+    }
+}
---
+++ part2/Crypotology/Lab3/test/java/SHA256Test.java
@@ -0,0 +1,128 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+
+import javax.xml.bind.DatatypeConverter;
+
+import cryptology.hash.SHA256;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+
+public class SHA256Test
+{
+    @Test
+    public void testHashGeneral() throws NoSuchAlgorithmException {
+        final int NUMBER_OF_TEST = 1000;
+        final int MAX_INPUT_LEN = 1000;
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        Random rand = new Random();
+
+        for(int i = 0; i < NUMBER_OF_TEST; ++i) {
+            byte[] input = new byte[rand.nextInt(MAX_INPUT_LEN)];
+            rand.nextBytes(input);
+
+            Assert.assertArrayEquals(SHA256.hash(input), digest.digest(input));
+        }
+    }
+
+
+    @Test
+    public void testHashEmpty() {
+        byte[] b = {};
+        byte[] expected = DatatypeConverter.parseHexBinary(
+                "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855");
+
+        assertArrayEquals(expected, SHA256.hash(b));
+    }
+
+    @Test
+    public void testHashRegular()
+    {
+        byte[] b = "Hello world!".getBytes(StandardCharsets.US_ASCII);
+        byte[] expected = DatatypeConverter.parseHexBinary(
+                "c0535e4be2b79ffd93291305436bf889314e4a3faec05ecffcbb7df31ad9e51a");
+
+        assertArrayEquals(expected, SHA256.hash(b));
+    }
+
+    @Test
+    public void testHashLong()
+    {
+        byte[] b = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+                + "Proin pulvinar turpis purus, sit amet dapibus magna commodo "
+                + "quis metus.").getBytes(StandardCharsets.US_ASCII);
+        byte[] expected = DatatypeConverter.parseHexBinary(
+                "60497604d2f6b4df42cea5efb8956f587f81a4ad66fa1b65d9e085224d255036");
+
+        assertArrayEquals(expected, SHA256.hash(b));
+    }
+
+    @Test
+    public void testHashRawBytes()
+    {
+        byte[] b = new byte[256];
+        for (int i = 0; i < b.length; ++i) {
+            b[i] = (byte) i;
+        }
+
+        byte[] expected = DatatypeConverter.parseHexBinary(
+                "40aff2e9d2d8922e47afd4648e6967497158785fbd1da870e7110266bf944880");
+
+        assertArrayEquals(expected, SHA256.hash(b));
+    }
+
+    @Test
+    public void testHash55()
+    {
+        byte[] b = new byte[55];
+        for (int i = 0; i < b.length; ++i) {
+            b[i] = (byte) 'a';
+        }
+
+        byte[] expected = DatatypeConverter.parseHexBinary(
+                "9f4390f8d30c2dd92ec9f095b65e2b9ae9b0a925a5258e241c9f1e910f734318");
+
+        assertArrayEquals(expected, SHA256.hash(b));
+    }
+
+    // PADDING
+    @Test
+    public void testPaddedLengthDivisibleBy512()
+    {
+        byte[] b = { 0, 1, 2, 3, 0 };
+
+        byte[] padded = SHA256.pad(b);
+        int paddedLengthBits = padded.length * 8;
+
+        assertTrue(paddedLengthBits % 512 == 0);
+    }
+
+    @Test
+    public void testPaddedMessageHas1Bit()
+    {
+        byte[] b = new byte[64];
+
+        byte[] padded = SHA256.pad(b);
+
+        assertEquals((byte) 0b1000_0000, padded[b.length]);
+    }
+
+    @Test
+    public void testPaddingAllZero()
+    {
+        byte[] b = { 1, 1, 1, 1, 1, 1, 1, };
+
+        byte[] padded = SHA256.pad(b);
+
+        for (int i = b.length + 1; i < padded.length - 8; ++i) {
+            assertEquals("byte " + i + " not 0", 0, padded[i]);
+        }
+    }
+
+}
